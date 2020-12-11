b25286e6c253e11779d44a3bfe7cea2422081f96
suric <suricatta1998@outlook.com>
2020-12-11 08:57:31 +0800

    添加toast

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -2,3 +2,32 @@ import Vue from 'vue'
 import Vuesax from 'vuesax'
 
 Vue.use(Vuesax)
+
+export default (ctx, inject) => {
+  const toast = (title = '', text = '', color = 'primary') => {
+    Vue.prototype.$vs.notification({
+      title,
+      text,
+      color,
+      position: 'top-right',
+      progress: 'auto'
+    })
+  }
+  inject('toast', {
+    primary (title, text) {
+      toast(title, text, 'primary')
+    },
+    success (title, text) {
+      toast(title, text, 'success')
+    },
+    danger (title, text) {
+      toast(title, text, 'danger')
+    },
+    warning (title, text) {
+      toast(title, text, 'warning')
+    },
+    dark (title, text) {
+      toast(title, text, 'dark')
+    }
+  })
+}
