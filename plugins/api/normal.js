9b3dfba62bcc335434d3a9767364fbbe06e4bc42
suric <suricatta1998@outlook.com>
2020-12-11 14:12:51 +0800

    添加省份和城市api

--- plugins/api/normal.js
+++ plugins/api/normal.js
@@ -8,5 +8,21 @@ export default request => ({
    */
   async getBanners () {
     return await request.get('banner')
+  },
+
+  /**
+   * 获取省份代码
+   *
+   */
+  async getProvinces () {
+    return await request.get('https://unpkg.com/province-city-china@7.0.0/dist/province.json')
+  },
+
+  /**
+   * 获取城市
+   */
+  async getCities () {
+    return await request.get('https://unpkg.com/province-city-china@7.0.0/dist/city.json')
   }
+
 })
