f78908d87897909a69b908d8b68dc6c4d79ee317
suric <suricatta1998@outlook.com>
2020-12-11 09:40:26 +0800

    客户端请求错误toast

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default ({ $axios, $toast }) => {
   $axios.onError((err) => {
     if (process.client && err.response.status !== 200) {
-      $toast.danger('请求失败', err.response)
+      $toast.danger('请求失败', err.response.msg)
     }
   })
 }
